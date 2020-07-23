3edfb9180608eef5cca772bcc9576900cf72c470
Mia Yan <68654939+yanbmia@users.noreply.github.com>
2020-07-22 22:54:41 -0400

    Add files via upload
    Mini Project 1

---
+++ Mini Project1/MiniProject1/MiniProject1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiniProject1
+//
+//  Created by Mia Yan on 7/22/20.
+//  Copyright © 2020 Mia Yan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Mini Project1/MiniProject1/MiniProject1Tests/MiniProject1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  MiniProject1Tests.swift
+//  MiniProject1Tests
+//
+//  Created by Mia Yan on 7/22/20.
+//  Copyright © 2020 Mia Yan. All rights reserved.
+//
+
+import XCTest
+@testable import MiniProject1
+
+class MiniProject1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
